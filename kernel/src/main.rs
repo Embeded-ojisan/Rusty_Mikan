f0723489475c3a08f3591f304b188840e9823ae7
lowlevelengineer <lowlevelengineer@gmail.com>
2023-05-22 01:31:00 +0900

    『初めてのカーネル(osbook_day03a)』の対応中

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,8 +1,19 @@
+#![no_std]
+#![no_main]
+//#![feature(asm)]
+
+use core::arch::asm;
+
 #[no_mangle]
-extern "C" fn kernel_main() {
+pub extern "C" fn kernel_main() {
     loop {
-        unsafe {
-            asm!("hlt");
-        }
+        unsafe { asm!("hlt") }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {
+        unsafe { asm!("hlt") }
     }
 }
