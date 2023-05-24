f58f8e8c7bec38714ecd71b3e7d7ab7227885332
lowlevelengineer <lowlevelengineer@gmail.com>
2023-05-24 23:36:03 +0900

    『初めてのカーネル(osbook_day03a)』の対応中

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -1,30 +1,46 @@
 #![no_main]
 #![no_std]
+//#![feature(alloc_error_handler)]
 
-use log::info;
+extern crate alloc;
+
 use uefi::prelude::*;
+use uefi::allocator::*;
 
-struct MemmoryMap {
-    buffer_size:,
-    buffer:         &mut[],
-    map_size:,
-    map_key:,
-    descriptor_size:,
-    descriptor_version:,
+use alloc::vec::Vec;
+use log::info;
+
+struct MemmoryMap<'a> {
+    buffer_size: usize,
+    buffer: Option<Vec<&'a u8>>,
+    map_size: usize,
+    map_key: usize,
+    descriptor_size: usize,
+    descriptor_version: usize,
 }
 
-impl MemmoryMap {
-    pub fn new {
-        ;
-    },
-
+impl<'a> MemmoryMap<'a> {
+    pub fn new(inp_buffer_size: usize) -> Self {
+        let mut buffer = Vec::with_capacity(inp_buffer_size);
+        buffer.resize(inp_buffer_size, &0u8);
+        MemmoryMap {
+            buffer_size: inp_buffer_size,
+            buffer: Some(buffer),
+            map_size: 0,
+            map_key: 0,
+            descriptor_size: 0,
+            descriptor_version: 0,
+        }
+    }
+/*
     pub fn GetMemoryMap(&self, system_table: SystemTable<Boot>) -> Status {
         system_table.memory_map(buffer);
     },
 
-    pub fn GetMemoryTypeUnicode(&self) -> {
+    pub fn GetMemoryTypeUnicode(&self) ->Self {
         ;
     },
+*/
 }
 
 #[entry]
@@ -34,6 +50,7 @@ fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     system_table.boot_services().stall(10_000_000);
 
     // メモリマップを取得
+    let mut memmap = MemmoryMap::new(4096*4);
 
     // カーネルファイルを読み出し
 
