28fa21f69b31543f13ea58417124057897af2cf5
lowlevelengineer <lowlevelengineer@gmail.com>
2023-05-27 23:05:17 +0900

    『初めてのカーネル(osbook_day03a)』の対応中

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -1,17 +1,37 @@
 #![no_main]
 #![no_std]
-//#![feature(alloc_error_handler)]
 
 extern crate alloc;
 
 use uefi::prelude::*;
 use uefi::allocator::*;
 
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::proto::device_path::{
+    text::DevicePathToText,
+    build::DevicePathBuilder,
+};
+use uefi::table::boot::{
+    SearchType,
+    ScopedProtocol,
+};
+use uefi::Identify;
+
+
 use alloc::vec;
 use alloc::vec::Vec;
 use log::info;
 
 use core::option::Option;
+
+/*
+use uefi::proto::device_path::text::{
+    AllowShortcuts, DevicePathToText, DisplayOnly,
+};
+use uefi::proto::loaded_image::LoadedImage;
+use uefi::table::boot::SearchType;
+use uefi::{Identify, Result};
+*/
 
 struct MemmoryMap {
     buffer_size: usize,
@@ -54,29 +74,76 @@ impl MemmoryMap {
             None => Status::BUFFER_TOO_SMALL,
         }
     }
-
 /*
-    pub fn GetMemoryTypeUnicode(&self) ->Self {
-        ;
-    }
-*/
-/*
-    pub fn SaveMemmoryMap(&self, system_table: SystemTable<Boot>)
-        -> SystemTable<Boot> {
+    pub fn SaveMemmoryMap(
+        &self, 
+        system_table: SystemTable<Boot>
+    )
+        -> Status {
             ;
     }
 */
 }
 
+
+struct EfiProtocols<'a> {
+    mSimpleFileSystem:      ScopedProtocol<'a, SimpleFileSystem>,
+    mDevicePathToText:      ScopedProtocol<'a, DevicePathToText>,
+//    mDevicePathBuilder:     ScopedProtocol<'a, DevicePathBuilder>,
+}
+
+impl<'a> EfiProtocols<'a> {
+    pub fn new(
+        boot_services: &'a uefi::prelude::BootServices,
+    ) -> Self {
+
+        EfiProtocols {
+            mSimpleFileSystem: 
+                boot_services
+                    .open_protocol_exclusive::<SimpleFileSystem>(
+                        boot_services.image_handle()
+                ).unwrap(),
+
+            mDevicePathToText:
+                boot_services
+                    .open_protocol_exclusive::<DevicePathToText>(
+                        boot_services.image_handle()
+                ).unwrap(),
+
+/*
+            mDevicePathBuilder:
+                boot_services
+                    .open_protocol_exclusive::<DevicePathBuilder>(
+                        boot_services.image_handle()
+                ).unwrap(),
+*/
+
+        }
+    }
+}
+/*
+fn OpenRootDir(
+    image_handle: Handle,
+    root: uefi::file
+) -> Status {
+    uefi
+
+
+    Status::SUCCESS
+}
+*/
+
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
     info!("Hello world!");
     system_table.boot_services().stall(10_000_000);
 
-    // メモリマップを取得
+    // 前処理
     let mut memmap = MemmoryMap::new(4096*4);
     memmap.GetMemoryMap(system_table);
+
+    // メモリマップを取得
 
     // カーネルファイルを読み出し
 
