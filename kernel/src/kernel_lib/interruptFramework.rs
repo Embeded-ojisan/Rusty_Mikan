29c40c4b452e0fc7a4e19ffc61337f7bc67940a5
lowlevelengineer <lowlevelengineer@gmail.com>
2023-10-22 11:55:45 +0900

    osbook_day07b

--- kernel/src/kernel_lib/interruptFramework.rs
+++ kernel/src/kernel_lib/interruptFramework.rs
@@ -1,3 +1,5 @@
+use bitfield::bitfield;
+
 #[repr(C, packed)]
 union InterruptDescriptorAttribute {
     data: u16,
@@ -6,21 +8,41 @@ union InterruptDescriptorAttribute {
 
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
-struct InterruptDescriptorBits {
+pub struct InterruptDescriptorBits {
     interrupt_stack_table: u16,
-    reserved1: u16,
+    reserved1: u16,                         // Use u16 or other integer type here
+
     type_: DescriptorType,
-    reserved2: u16,
-    descriptor_privilege_level: u16,
-    present: u16,
+
+    reserved2: u16,                         // Use u16 or other integer type here
+    descriptor_privilege_level: u8,         // Use u8 or other integer type here
+    present: bool,                          // Use bool for a 1-bit field
 }
 
-
-#[repr(u16)]
-#[derive(Debug, Copy, Clone)]
-enum DescriptorType { 
-    Type1 = 1,
-    Type2 = 2,
-    Type3 = 3,
-    // Add more enum variants as needed
+bitfield! {
+    #[derive(Copy, Clone)]
+    pub struct DescriptorType(u16);
+    impl Debug;
+    u16;
+    KUpper8Bytes, set_kupper8bytes: 0;
+    KLdt, set_kldt: 2;
+    KTSSAvailable, set_ktssavailable: 9;
+    KTSSBusy, set_ktssbusy: 11;
+    KCallGate, set_kcallgate: 12;
+    KInterruptGate, set_kinterruptgate: 14;
+    KTrapGate, set_ktrapgate: 15;
+    KGuard, set_kguard: 16;
 }
+
+/*
+impl InterruptDescriptorAttribute {
+    fn MakeIDTAttr(
+        mut self,
+        type: DescriptorType,
+        descriptor_privilege_level: u16,
+        present: 
+    ) -> Self {
+
+    }
+}
+*/
